a9409ca7e284d5ea8e5d1506fe9d5c6025f194ff
Mario Kernich <mario.kernich@swan.de>
2022-11-18 23:49:58 +0100

    initial

---
+++ plugin-base/plugin-base.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: WordPress Plugin Template
+ * Description: A WordPress plugin template
+ * Author: Mario Kernich
+ * Version: 1.0.0
+ * Author URI: https://marioke.dev
+ * Text Domain: plugin-base
+ * Domain Path: /languages
+ * Requires at least: 6.0
+ * Requires PHP: 8.0
+ * Repository: https://github.com/marioke/WordPress-Plugin-Template
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // exit if accessed directly
+}
